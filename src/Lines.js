2847850ba5a43fc63e112a3623dfc3eeaf09e361
Den Pol <selection4test@gmail.com>
2019-05-31 22:07:56 +0300

    Updates

--- src/Lines.js
+++ src/Lines.js
@@ -4,6 +4,7 @@ import {
   by3Points,
 } from 'get-parabola';
 import { getKB } from 'interpolate-by-pravosleva';
+
 import {
   enthalpy,
   // humidity,
@@ -30,13 +31,6 @@ export default class Lines {
   static _getHumidityLines() {
     const coeffsArr = [];
     const pointsArrs = [];
-    // const temperatureTemplateArr = [
-    //   -41, -40, -39, -38, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28,
-    //   -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13,
-    //   -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7,
-    //   8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    //   27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41
-    // ];
     const temperatureTemplateArr = [-41, 0, 41];
     const fiValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
@@ -50,7 +44,6 @@ export default class Lines {
         });
       }
     }
-    // console.log(pointsArrs[9]);
     // pointsArrs.map(points => coeffsArr.push(byLeastSquaresApproximation(points)));
     pointsArrs.map(points => coeffsArr.push(by3Points(getNormalized3Points(points))));
 
